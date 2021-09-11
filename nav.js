cd47a226b0c3a93bfab77a8ac646312f2b282a52
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-09-11 09:12:30 -0400

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("#toggle")
+const nav = document.querySelector(".nav__container")
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("clicked")
+
+})
+
+
+
+
+
