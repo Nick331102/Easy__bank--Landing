ed6883ad31c37ef1729d9eeda47a3138e29b0fe0
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-09-18 22:53:37 -0400

    Add files via upload
    Improved hamburger menu

--- nav.js
+++ nav.js
@@ -1,11 +1,10 @@
 const menu = document.querySelector("#toggle")
 const nav = document.querySelector(".nav__links--container")
-const lines = document.querySelector(".line")
 
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("clicked")
-    lines.classList.toggle("clicked")
+    menu.classList.toggle("clicked")
 })
 
 
