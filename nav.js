2cdbb4228ca91659f26319667bebf57936a7ba75
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-11-15 07:24:38 -0500

    Add files via upload
    dark overlay added for hamburger menu clicked

--- nav.js
+++ nav.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector("#toggle")
 const nav = document.querySelector(".nav__links--container")
+const overlay = document.querySelector(".clicked__overlay")
 
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("clicked")
     menu.classList.toggle("clicked")
+    overlay.classList.toggle("show")
 })
 
 
